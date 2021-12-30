6c8a3c7cca2bba8b7b99f11b44370dc704a0bcfa
StarkaLukas <lstarka141000@gmail.com>
2021-12-30 18:02:16 +0100

    create sell-date service with getter and setter

---
+++ frontend/src/app/shared/sell-date.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {ITree} from "./backend.service";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SellDateService {
+
+    private treeToSell: ITree | null;
+
+    public constructor() {
+        this.treeToSell = null;
+    }
+
+    public set tree(tree: ITree | null) {
+        this.treeToSell = tree;
+    }
+
+    public get tree(): ITree | null {
+        return this.treeToSell;
+    }
+}
