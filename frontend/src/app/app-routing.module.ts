57c331264c35076701f57fac03e08aca16a98b3f
StarkaLukas <lstarka141000@gmail.com>
2021-12-30 18:15:09 +0100

    add new route to sale page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {TreeComponent} from "./tree/tree.component";
+import {SaleComponent} from "./tree/sale/sale.component";
 
 const routes: Routes = [
     {path: 'tree', component: TreeComponent},
+    {path: 'sale', component: SaleComponent},
     {path: '**', component: HomeComponent, pathMatch: 'full'}
 ];
 
