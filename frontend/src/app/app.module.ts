bd101e5e8801ea640656c95b0e05b63aa93a6d5e
StarkaLukas <lstarka141000@gmail.com>
2022-01-03 15:14:01 +0100

    import ReactiveFormsModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,16 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule} from "@angular/material/card";
+import { MatCardModule } from "@angular/material/card";
 import { HomeComponent } from './home/home.component';
 import { TreeComponent } from './tree/tree.component';
-import {MatButtonModule} from "@angular/material/button";
-import {HttpClientModule} from "@angular/common/http";
+import { MatButtonModule } from "@angular/material/button";
+import { HttpClientModule } from "@angular/common/http";
 import { TypeSelectionComponent } from './tree/type-selection/type-selection.component';
-import {MatSelectModule} from "@angular/material/select";
+import { MatSelectModule } from "@angular/material/select";
 import { TreeListComponent } from './tree/tree-list/tree-list.component';
 import { TreeDisplayComponent } from './tree/tree-display/tree-display.component';
 import { SaleComponent } from './tree/sale/sale.component';
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { SaleComponent } from './tree/sale/sale.component';
         MatCardModule,
         MatButtonModule,
         HttpClientModule,
-        MatSelectModule
+        MatSelectModule,
+        ReactiveFormsModule
     ],
   providers: [],
   bootstrap: [AppComponent]
