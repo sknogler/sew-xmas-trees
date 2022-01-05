9317dadb6df2620bcf7c4e272d5932800aa0cf54
StarkaLukas <lstarka141000@gmail.com>
2022-01-05 17:54:36 +0100

    inject dataService and backendService

--- frontend/src/app/tree/paypal/paypal.component.ts
+++ frontend/src/app/tree/paypal/paypal.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
 import {Router} from "@angular/router";
+import {SellDateService} from "../../shared/sell-date.service";
+import {BackendService, ITree} from "../../shared/backend.service";
 
 @Component({
     selector: 'app-paypal',
@@ -14,10 +16,15 @@ export class PaypalComponent {
         password: [null, Validators.required],
     });
 
+    public tree: ITree;
+
     constructor(
         private fb: FormBuilder,
-        private router: Router
+        private router: Router,
+        private readonly dataService: SellDateService,
+        private readonly backendService: BackendService
     ) {
+        this.tree = dataService.tree!;
     }
 
     ngOnInit(): void {
